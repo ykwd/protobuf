059f10f0e86861be57f85ca1979a2973426e9256
imzhenyu <imzhenyu@outlook.com>
2015-04-13 22:17:51 +0800

    add support for rDSN code generation (see project robust Distributed System Necleus for details)

--- src/google/protobuf/compiler/main.cc
+++ src/google/protobuf/compiler/main.cc
@@ -36,6 +36,7 @@
 #include <google/protobuf/compiler/java/java_generator.h>
 #include <google/protobuf/compiler/javanano/javanano_generator.h>
 #include <google/protobuf/compiler/ruby/ruby_generator.h>
+#include <google/protobuf/compiler/rdsn/rdsn_generator.h>
 
 int main(int argc, char* argv[]) {
 
@@ -67,6 +68,11 @@ int main(int argc, char* argv[]) {
   google::protobuf::compiler::ruby::Generator rb_generator;
   cli.RegisterGenerator("--ruby_out", &rb_generator,
                         "Generate Ruby source file.");
+
+  // rDSN
+  google::protobuf::compiler::rdsn::RdsnGenerator rdsn_generator;
+  cli.RegisterGenerator("--rdsn_out", &rdsn_generator,
+      "Generate rDSN source file.");
 
   return cli.Run(argc, argv);
 }
